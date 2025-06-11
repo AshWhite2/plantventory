df4a96404266eb75cb0796209ce3ad7949aa8e74
AshWhite2 <hanif.indra.kusuma@gmail.com>
2025-06-11 22:33:30 +0700

    com

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,12 +12,13 @@ const Hero = () => {
             Just released v1.0.0
           </Badge>
           <h1 className="mt-6 max-w-[17ch] text-4xl md:text-5xl lg:text-[2.75rem] xl:text-5xl font-bold !leading-[1.2] tracking-tight">
-            Customized Shadcn UI Blocks & Components
+            Plantventory for tracing your plant life cycle
           </h1>
           <p className="mt-6 max-w-[60ch] text-lg">
-            Explore a collection of Shadcn UI blocks and components, ready to
-            preview and copy. Streamline your development workflow with
-            easy-to-implement examples.
+            Transform your home with our lush, air-purifying Monstera Deliciosa!
+            This low-maintenance, tropical beauty features vibrant, split-leaf
+            foliage—perfect for modern spaces. Pet-friendly and thriving in
+            indirect light, it’s a must-have for plant lovers.
           </p>
           <div className="mt-12 flex items-center gap-4">
             <Button size="lg" className="rounded-full text-base">
--- src/stack.tsx
+++ src/stack.tsx
@@ -1,7 +1,8 @@
 import "server-only";
-
 import { StackServerApp } from "@stackframe/stack";
 
 export const stackServerApp = new StackServerApp({
   tokenStore: "nextjs-cookie",
+  projectId: process.env.NEXT_PUBLIC_STACK_PROJECT_ID,
+
 });
