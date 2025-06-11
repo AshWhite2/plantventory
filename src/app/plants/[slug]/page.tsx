bb0353da731382f66cee9067b96421193de4fc73
AshWhite2 <hanif.indra.kusuma@gmail.com>
2025-06-11 22:42:19 +0700

    p

--- src/app/plants/[slug]/page.tsx
+++ src/app/plants/[slug]/page.tsx
@@ -1,42 +1,47 @@
-import React from 'react'
-import PlantCard from './PlantCard'
-import { getPlantById } from '@/actions/plant.aciton';
+import React from 'react';
+import PlantCard from './PlantCard';
+import { getPlantById } from '@/actions/plant.action';
 import { stackServerApp } from '@/stack';
 import { SignIn } from '@stackframe/stack';
 
-
-export async function generateMetadata({
-    params,
-  }: {
-    params: { slug: string };
-  }) {
-   
-    // Extract the id from the slug by splitting on the delimiter
-    const [id] = params.slug.split("--");
-    const plant = await getPlantById(id);
-    return {
-      title: plant ? plant.name : "Plant Details",
-      description: plant ? plant.description : "Plant details page",
-    };
+interface MetadataProps {
+  params: { slug: string };
 }
+
+interface PageProps {
+  params: { slug: string };
+}
+
+export async function generateMetadata({ params }: MetadataProps) {
+  const [id] = params.slug.split("--");
+  const plant = await getPlantById(id);
   
+  return {
+    title: plant?.name || "Plant Details",
+    description: plant?.description || "Plant details page",
+  };
+}
 
-async function page({params}: {params: {slug: string}}) {
-    const user = await stackServerApp.getUser();
-    const [id] = params.slug.split("--");
-    const plant = await getPlantById(id);
+async function Page({ params }: PageProps) {
+  const user = await stackServerApp.getUser();
+  const [id] = params.slug.split("--");
+  const plant = await getPlantById(id);
 
-    if(!user) {
-        return <SignIn />
-    }
+  if (!user) {
+    return <SignIn />;
+  }
+
+  if (!plant) {
+    return <div>Plant not found</div>;
+  }
 
   return (
-     <div className="mt-7 max-w-7xl mx-auto px-4 grid grid-cols-1 lg:grid-cols-10 gap-6">
-              <div className="lg:col-span-full">
-                <PlantCard plant={plant} />
-              </div>
-            </div>
-  )
+    <div className="mt-7 max-w-7xl mx-auto px-4 grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-full">
+        <PlantCard plant={plant} />
+      </div>
+    </div>
+  );
 }
 
-export default page
+export default Page;
